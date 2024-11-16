740f64e982efb9d3e9c35e654603a34feae802cc
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-17 01:54:11 +0545

    homework-1-voter-eligible

---
+++ voter-eligible/voter-eligible.c
@@ -0,0 +1,17 @@
+/* WAP that asks the user to input their age
+and print if they are eligible to vote or not */
+
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Welcome to Voting Center\n");
+    printf("Please enter your age: ");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("Congrats! You are eligible to vote.");
+    }else{
+        printf("Sorry! You are not eligible to vote.");
+    }
+    printf("\nThanks for visiting Voting Center");
+    return 0;
+}
