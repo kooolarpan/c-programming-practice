b2234762178778d35f5485133cecf6ddf0a358e8
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-17 15:01:56 +0545

    else-if

---
+++ else-if/else-if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a positive number: ");
+    scanf("%d",&a);
+    if(a<10){
+        printf("Apple");
+    }else if(a>=10&&a<20){
+        printf("Ball");
+    }else if(a>=20&&a<30){
+        printf("Cat");
+    }else{
+        printf("Dog");
+    }
+    return 0;
+}
