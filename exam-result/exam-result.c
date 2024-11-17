b57f6ca7f052f32732f92f3cd38b0f741bf07182
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-17 20:41:32 +0545

    exam-result

---
+++ exam-result/exam-result.c
@@ -0,0 +1,28 @@
+/* WAP that asks the user to input their obtained percentage in exam
+and display their division based on the following table. */
+
+/* Percentage                                      -        Division
+   greater than or equal to 80                              Distinction
+   less than 80 and greater than or equal to 60             First Division
+   less than 60 and greater than or equal to 40             Second Division
+   Less than 40                                             Fail
+ */
+
+#include <stdio.h>
+int main(){
+    int obtainedPercentage;
+    printf("=== Welcome to Exam Result Portal ===\n");
+    printf("Enter your obtained percentage: ");
+    scanf("%d",&obtainedPercentage);
+    if(obtainedPercentage>=80){
+        printf("Congrats! You have secured distinction");
+    }else if(obtainedPercentage<80 && obtainedPercentage>=60){
+        printf("Congrats! You have secured first division");
+    }else if(obtainedPercentage<60 && obtainedPercentage>=40){
+        printf("Congrats! You have secured second division");
+    }else{
+        printf("Sorry, You have failed");
+    }
+    printf("\n=== Thanks for visiting Exam Result Portal ===");
+    return 0;
+}
