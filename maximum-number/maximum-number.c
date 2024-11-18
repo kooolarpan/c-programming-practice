6e6c3ab2153a4d1d498c56c28b6fe080e9870633
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-18 16:07:24 +0545

    maximum number

---
+++ maximum-number/maximum-number.c
@@ -0,0 +1,17 @@
+/* WAP that asks the user to input three different numbers 
+and print the maximum among them */
+
+#include <stdio.h>
+int main(){
+    int firstNumber, secondNumber, thirdNumber;
+    printf("Enter any three numbers: ");
+    scanf("%d%d%d",&firstNumber,&secondNumber,&thirdNumber);
+    if(firstNumber>secondNumber&&firstNumber>thirdNumber){
+        printf("%d is largest",firstNumber);
+    }else if(secondNumber>firstNumber && secondNumber>thirdNumber){
+        printf("%d is largest",secondNumber);
+    }else{
+        printf("%d is largest",thirdNumber);
+    }
+    return 0;
+}
