0d12ad2f25fee0221802ae7bc7ab20b86852767c
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-18 15:45:00 +0545

    update-exam-result

--- exam-result/exam-result.c
+++ exam-result/exam-result.c
@@ -14,15 +14,19 @@ int main(){
     printf("=== Welcome to Exam Result Portal ===\n");
     printf("Enter your obtained percentage: ");
     scanf("%d",&obtainedPercentage);
-    if(obtainedPercentage>=80){
-        printf("Congrats! You have secured distinction");
-    }else if(obtainedPercentage<80 && obtainedPercentage>=60){
-        printf("Congrats! You have secured first division");
-    }else if(obtainedPercentage<60 && obtainedPercentage>=40){
-        printf("Congrats! You have secured second division");
+    if(obtainedPercentage<=100){
+        if(obtainedPercentage>=80){
+            printf("Congrats! You have secured distinction");
+        }else if(obtainedPercentage<80 && obtainedPercentage>=60){
+            printf("Congrats! You have secured first division");
+        }else if(obtainedPercentage<60 && obtainedPercentage>=40){
+            printf("Congrats! You have secured second division");
+        }else{
+            printf("Sorry, You have failed");
+        }
     }else{
-        printf("Sorry, You have failed");
-    }
+        printf("Sorry! The provided percentage is greater than 100");
+    }        
     printf("\n=== Thanks for visiting Exam Result Portal ===");
     return 0;
 }
