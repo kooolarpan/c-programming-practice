d11bd71fdd22fd0fc5b57dcaf202db75aee7c970
Kool Arpan <koolrapperarpan@gmail.com>
2024-11-19 22:05:38 +0545

    switch-statement

---
+++ switch-statement/switch-statement.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter a number to walk the robot: ");
+    scanf("%d",&number);
+    switch (number)
+    {
+    case 1:
+        printf("Walk straight");
+        break;
+
+    case 2:
+        printf("Turn right");
+        break;
+        
+    case 3:
+        printf("Turn left");
+        break;
+    
+    default:
+        printf("Stop");
+        break;
+    }
+    return 0;
+}
