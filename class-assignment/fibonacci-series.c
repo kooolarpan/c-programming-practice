6b0471636c35da7ef4d66a64737e3b3aca75128a
kooolarpan <officialkoolarpan@gmail.com>
2025-01-19 15:54:12 +0545

    edit: fibonacci

--- class-assignment/fibonacci-series.c
+++ class-assignment/fibonacci-series.c
@@ -1,11 +1,12 @@
 // WAP to print the fibonacci series up to 10th term
-// 1, 1, 2, 3, 5, 8, 13, 21
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 
 #include <stdio.h>
 int main() {
     int a = 0, b = 1, sum;
+    printf("%d\t", a);
     printf("%d\t", b);
-    for (int i = 1; i <= 9; i++) {
+    for (int i = 0; i < 8; i++) {
         sum = a + b;
         a = b;
         b = sum;
